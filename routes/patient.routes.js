0dc5420bec236609580ea3514c31fd5e37c13dc8
Syed25794 <syedgulmohammad25794@gmail.com>
2023-01-18 13:15:39 +0530

    assignment done

--- routes/patient.routes.js
+++ routes/patient.routes.js
@@ -2,8 +2,10 @@ const Router = require("express");
 
 const  registerPatient  = require("../controllers/patient.controller");
 
+const { validateInputsPatient } = require("../middlewares/validation");
+
 const patientRoutes = Router();
 
-patientRoutes.post("/registration",registerPatient);
+patientRoutes.post("/registration",validateInputsPatient,registerPatient);
 
 module.exports = patientRoutes;
--- routes/psychiatrist.routes.js
+++ routes/psychiatrist.routes.js
@@ -2,8 +2,10 @@ const Router = require("express");
 
 const getAllpsychiatrists = require("../controllers/psychiatrist.controller");
 
+const { getPsychiatristValidate } = require("../middlewares/validation");
+
 const psychiatristRoutes = Router();
 
-psychiatristRoutes.get("/all",getAllpsychiatrists);
+psychiatristRoutes.get("/all",getPsychiatristValidate,getAllpsychiatrists);
 
 module.exports=psychiatristRoutes;
